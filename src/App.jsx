77d89211ada79ada1ad127421a78d4098ea2205b
Sammyfrosh35 <badejosamuel9@gmail.com>
2024-10-29 22:07:33 +0100

    new page

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Hero from './Components/Hero'
+import Features from './Components/Features'
+import Workflow from './Components/workflow'
+import Pricing from './Components/Pricing'
+import Testimonial from './Components/Testimonial'
+import Footer from './Components/Footer'
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <Hero />
+        <Features />
+        <Workflow />
+        <Pricing />
+        <Testimonial />
+        <Footer />
+      </div>
+    </div>
+  )
+}
+
+export default App
